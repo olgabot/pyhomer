aff3329f7b62fccc9cf371db9e10c4b8d3b030eb
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-12-03 19:33:26 -0800

    Move import

--- pyhomer/__init__.py
+++ pyhomer/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from .pyhomer import ForegroundBackgroundPair, construct_homer_command
+
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
-from .pyhomer import ForegroundBackgroundPair, construct_homer_command
 
 __all__ = ['ForegroundBackgroundPair', 'construct_homer_command']
