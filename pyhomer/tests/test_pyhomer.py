816ef22de2dd075c27bb1e94cea1b96a980848e6
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-12-03 17:49:27 -0800

    initial commit of tests

--- pyhomer/tests/test_pyhomer.py
+++ pyhomer/tests/test_pyhomer.py
@@ -8,21 +8,77 @@ test_pyhomer
 Tests for `pyhomer` module.
 """
 
-import unittest
+import os
 
-from pyhomer import pyhomer
+import pytest
+import pybedtools
+
+@pytest.fixture
+def foreground_filename():
+    dirname = os.path.dirname(__file__)
+    return  '{}/foreground.bed'.format(dirname)
 
 
-class TestPyhomer(unittest.TestCase):
+@pytest.fixture(params=['BedTool', 'filename'])
+def foreground(request, foreground_filename):
+    if request.param == 'filename':
+        return foreground_filename
+    elif request.param == 'BedTool':
+        return pybedtools.BedTool(foreground_filename)
 
-    def setUp(self):
-        pass
+@pytest.fixture
+def background_filename():
+    dirname = os.path.dirname(__file__)
+    return  '{}/background.bed'.format(dirname)
 
-    def test_something(self):
-        pass
 
-    def tearDown(self):
-        pass
+@pytest.fixture(params=['BedTool', 'filename'])
+def background(request, background_filename):
+    if request.param == 'filename':
+        return background_filename
+    elif request.param == 'BedTool':
+        return pybedtools.BedTool(background_filename)
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture(params=[None, "doesn't exist", "already exists"])
+def out_dir(request):
+    if request.param is None:
+        return None
+    elif request.param == "doesn't exist":
+        return 'homer/'
+    elif request.param == "already exists":
+        return './'
+
+
+@pytest.fixture(params=[None, '-rna -len 4,5,6 -mset vertebrates -mis 0 -p 4 '
+                              '-noweight'])
+def flags(request):
+    return request.param
+
+
+@pytest.fixture(params=[None, '~/bin/findMotifsGenome.pl'])
+def findMotifsGenome(request):
+    return request.param
+
+
+@pytest.fixture(params=[True, False])
+def force(request):
+    return request.param
+
+
+@pytest.fixture(params=[None, 'mm10'])
+def genome(request):
+    return request.param
+
+
+def test_construct_homer_command(foreground_filename, background_filename,
+                                 flags, findMotifsGenome, out_dir, force,
+                                 genome):
+    import pyhomer
+
+    kwargs = dict(flags=flags, findMotifsGenome=findMotifsGenome,
+                  out_dir=out_dir, force=force, genome=genome)
+    kwargs = dict(filter(lambda x: x[1] is not None, kwargs.items()))
+
+    test = pyhomer.construct_homer_command(
+        foreground_filename, background_filename, **kwargs)
+    assert False
